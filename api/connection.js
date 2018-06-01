cb5173a882e9c5f67768890cd59226547770c2b6
kaushalmadani <31029621+kaushalmadani@users.noreply.github.com>
2018-06-01 16:18:28 +0530

    Update connection.js

--- api/connection.js
+++ api/connection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'ec2-107-20-133-82.compute-1.amazonaws.com',
+  host     : 'ec2-107-20-133-82.compute-1.amazonaws.com:5432',
   user     : 'kixlkcoaclciri',
   password : '88bb2e09fa106acde9ba1dca72f10b49eee12f031b6ada7fe88f32f33572e3cd',
   database : 'dbfj5rs69grbmu',
